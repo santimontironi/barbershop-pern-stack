04aa974d7d01027cc2b4578c8fcb5b7e5eef3cb7
Santiago Montironi <santiimontironi@gmail.com>
2026-04-04 20:47:04 -0300

    config para produccion

--- backend/config/bd.js
+++ backend/config/bd.js
@@ -5,12 +5,11 @@ dotenv.config()
 
 const { Pool } = pkg
 
+const isProduction = process.env.NODE_ENV === 'production'
+
 const db = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: isProduction ? { rejectUnauthorized: false } : false
 })
 
 export default db
